6e4fed0d4a894b2fc000c73f9ddaa341609b564c
Norberto <nbeto@users.noreply.github.com>
2018-08-23 11:55:52 +0100

    Update woocommerce_checkout_fields.php

--- woocommerce_checkout_fields.php
+++ woocommerce_checkout_fields.php
@@ -3,9 +3,8 @@
 //
 // Remove "Company" field from woocommerce checkout.
 //
-add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
-function custom_override_checkout_fields( $fields ) {
-    unset($fields['billing']['billing_company']);
+add_filter( 'woocommerce_checkout_fields' , 'nb_unset_company_checkout_field' );
+function nb_unset_company_checkout_field( $fields ) {
     unset($fields['billing']['billing_company']);
     return $fields;
 }
